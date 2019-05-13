f5ce5b9ad345f80240577c6e67ba91bc35c8e9dd
Gennadii_Borodin <Gennadii_Borodin@epam.com>
2019-05-13 12:31:31 +0400

    lombok

--- src/main/java/com/home/model/Note.java
+++ src/main/java/com/home/model/Note.java
@@ -1,81 +1,25 @@
 package com.home.model;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "note")
 public class Note {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Setter(value = AccessLevel.NONE)
     private Long id;
     private BigDecimal amount;
     private String description;
     private LocalDate noteDate;
 
-    public Note() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getnDate() {
-        return noteDate;
-    }
-
-    public void setnDate(LocalDate noteDate) {
-        this.noteDate = noteDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Note{" +
-                "id=" + id +
-                ", amount=" + amount +
-                ", description='" + description + '\'' +
-                ", noteDate=" + noteDate +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Note note = (Note) o;
-        return Objects.equals(id, note.id) &&
-                Objects.equals(amount, note.amount) &&
-                Objects.equals(description, note.description) &&
-                Objects.equals(noteDate, note.noteDate);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(id, amount, description, noteDate);
-    }
 }
